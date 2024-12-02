004707449c19947042848c0fe891f630911520d3
mohamedAdhamc <mohamed.adhamnagiub@gmail.com>
2024-12-02 18:14:38 +0200

    finish get_possible_locations for grasshopper

--- utils/grass_hopper.py
+++ utils/grass_hopper.py
@@ -1,5 +1,6 @@
 from location import Location
 from game_object import GameObject
+from board import Board
 
 class Grasshopper(GameObject):
     def __init__(self, location):
@@ -14,6 +15,64 @@ class Grasshopper(GameObject):
     def __repr__(self):
         return f"Grasshopper at {self.get_location()}"
     
-    def get_next_possible_locations(self):
-        #if there is an element above us
-        return
+    def get_next_possible_locations(self, board:Board):
+        newLocations = []
+
+        loc = self.get_location()
+        x = loc.get_x()
+        y = loc.get_y()
+        
+        #if there is an element above and right
+        if (Board.get_object(Location(x+1, y+1)) is not None):
+            new_x = x
+            new_y = y
+            while (Board.get_object(Location(x+1, y+1)) is not None):
+                new_x += 1
+                new_y += 1
+            newLocations.append(Location(new_x,new_y))
+            
+
+        #if there is an element above and left
+        if (Board.get_object(Location(x-1, y+1)) is not None):
+            new_x = x
+            new_y = y
+            while (Board.get_object(Location(x-1, y+1)) is not None):
+                new_x -= 1
+                new_y += 1
+            newLocations.append(Location(new_x,new_y))
+
+        #if there is an element bottom and right
+        if (Board.get_object(Location(x+1, y-1)) is not None):
+            new_x = x
+            new_y = y
+            while (Board.get_object(Location(x+1, y-1)) is not None):
+                new_x += 1
+                new_y -= 1
+            newLocations.append(Location(new_x, new_y))
+
+        #if there is an element bottom and left
+        if (Board.get_object(Location(x-1, y-1)) is not None):
+            new_x = x
+            new_y = y
+            while (Board.get_object(Location(x-1, y-1)) is not None):
+                new_x -= 1
+                new_y -= 1
+            newLocations.append(Location(new_x, new_y))
+
+        #if there is an element to the right
+        if (Board.get_object(Location(x+2, y)) is not None):
+            new_x = x + 2
+            new_y = y
+            while (Board.get_object(Location(x-1, y-1)) is not None):
+                new_x += 2
+            newLocations.append(Location(new_x, new_y))
+
+        #if there is an element to the left
+        if (Board.get_object(Location(x-2, y)) is not None):
+            new_x = x - 2
+            new_y = y
+            while (Board.get_object(Location(x-1, y-1)) is not None):
+                new_x -= 2
+            newLocations.append(Location(new_x, new_y))
+
+        return newLocations
