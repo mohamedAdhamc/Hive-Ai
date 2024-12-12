3d09ba8662cddd89e10a10a8f2e56494f322a0e7
Ali Mohsen <ali_m_2001@hotmail.com>
2024-12-12 15:35:49 +0200

    add test tree method for ai algo testing

---
+++ utils/state_tree_node.py
@@ -0,0 +1,19 @@
+class StateTreeNode:
+    
+    def __init__(self, parent = None, move = None, evaluation = 0, depth = 0):
+        self.evaluation = evaluation
+        self.move = move
+        self.children = []
+        self.parent = parent
+        self.depth = depth
+
+    def print_tree(self):
+        if self.children == []:
+            print("|   " * (self.depth-1) + "|-> ", end="")
+            print(self.move, self.evaluation)
+            return
+        if self.depth > 0:
+            print("|   " * (self.depth-1) + "|-> ", end="")
+            print(self.move)
+        for child in self.children:
+            child.print_tree()
